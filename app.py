cbf9ea13c36db2ef2f8fbb8b1e3f123544a88cfe
Mohamed-Fikry87 <136803555+Mohamed-Fikry87@users.noreply.github.com>
2024-02-02 15:20:41 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('Hello World')
+st.title('Hello World')
+st.title('Hello World')
+st.image("https://cdn.shopify.com/s/files/1/1993/5303/files/toddler_boy_new_arrivals_x800_b7c2ab86-312d-4665-a9a2-dbaf99454869_1200x.jpg")
